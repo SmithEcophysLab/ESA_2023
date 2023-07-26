3d017f1ef5f00570686173b78f71363aefd71b70
SmithEcophysLab <nick.gregory.smith@gmail.com>
2023-07-26 12:03:55 -0500

    some more updates to the talk

---
+++ presentation/analysis/c4_fig.R
@@ -0,0 +1,9 @@
+# c4_fig.R
+## create a simple c4 figure for ESA 2023 talk
+
+## source model
+source('../../../optimal_vcmax_R/calc_optimal_vcmax.R')
+sourceDirectory('../../../optimal_vcmax_R/functions')
+
+## run model
+calc_optimal_vcmax()
